f43646914f9b96ea55a2af4997706c5f101dee2f
Pedro Miranda <pbragamiranda@gmail.com>
2020-10-14 18:41:26 -0300

    commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+
+  def create
+
+  end
+
 end
+
+
+# <!--   <%= link_to "Edit Product", "#", id: "yourButton", class: "myclass" , pid: "#{product.id}", remote: true %> -->
+
+# <!-- <div id="myForm">
+#     <%= render "shared/modal", product: @new_product %>
+# </div>
+#  -->
